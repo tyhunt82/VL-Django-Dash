ab5c9b3b2b0a036c1559588445c89939f483beb1
Tyler Hunt <tyhunt82@users.noreply.github.com>
2023-10-08 16:29:22 +0000

    update all

--- DjangoDash/DjangoDash/settings.py
+++ DjangoDash/DjangoDash/settings.py
@@ -37,8 +37,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
     'dashapp1',
 ]
 
