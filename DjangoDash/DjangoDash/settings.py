2e2ba0a310a89d0d4022468a7346a1d6ef671daa
Tyler Hunt <tyhunt82@users.noreply.github.com>
2023-10-08 16:22:58 +0000

    urls.py

--- DjangoDash/DjangoDash/settings.py
+++ DjangoDash/DjangoDash/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'dashapp1',
 ]
 
 MIDDLEWARE = [
--- DjangoDash/dashapp1/views.py
+++ DjangoDash/dashapp1/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('dashboard')
+    return render(request, 'login.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+def signup_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        User.objects.create_user(username=username, password=password)
+        return redirect('login')
+    return render(request, 'signup.html')
+
+@login_required
+def dashboard_view(request):
+    return render(request, 'dashboard.html')
