0626b27c7aa2678cd7a9e95ae0fca719e16a48a6
Tyler Hunt <tyhunt82@users.noreply.github.com>
2023-10-08 16:40:41 +0000

    re

--- DjangoDash/DjangoDash/urls.py
+++ DjangoDash/DjangoDash/urls.py
@@ -17,8 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include, path
+from django.shortcuts import redirect
+
+def redirect_to_login(request):
+    return redirect('login')
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('', redirect_to_login, name='home'),
     path('', include('dashapp1.urls')), 
 ]
